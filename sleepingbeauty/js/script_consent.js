887d95bb0e725951c83a1ef0b701f06917d2d5b8
corytimmons <43824202+corytimmons@users.noreply.github.com>
2019-05-29 19:44:20 -0700

    updating some pages and content

--- sleepingbeauty/js/script_consent.js
+++ sleepingbeauty/js/script_consent.js
@@ -8,37 +8,49 @@ $(".consenta").click(function(){
 
 $(".consentb").click(function(){
     $(".consentc").show();
+    $(".consenta").css("color", "black");
+    $(".consentb").css("color", "black");
   });
 
 $(".consentc").click(function(){
     $(".consentd").show();
+       $(".consentc").css("color", "black");
   });
 $(".consentd").click(function(){
     $(".consente").show();
+       $(".consentd").css("color", "black");
   });
 $(".consente").click(function(){
     $(".consentf").show();
+    $(".consente").css("color", "black");
   });
 $(".consentf").click(function(){
     $(".consentg").show();
+    $(".consentf").css("color", "black");
   });
 $(".consentg").click(function(){
     $(".consenth").show();
+    $(".consentg").css("color", "black");
   });
 $(".consenth").click(function(){
     $(".consenti").show();
+    $(".consenth").css("color", "black");
   });
 $(".consenti").click(function(){
     $(".consentj").show();
+    $(".consenti").css("color", "black");
   });
 $(".consentj").click(function(){
     $(".consentk").show();
+    $(".consentj").css("color", "black");
   });
 $(".consentk").click(function(){
     $(".consentl").show();
+    $(".consentk").css("color", "black");
   });
 $(".consentl").click(function(){
     $(".consentm").show();
+    $(".consentl").css("color", "black");
   });
 
   });
